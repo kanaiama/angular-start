31b64ad5e8015b1a4aa952a7d9a544e48528244b
kanaiama <wilson.kanaiamajr@sptech.school>
2023-04-17 11:35:45 -0300

    ADD: directive marca-texto alterando cor de fundo e cor do texto

--- diretivasAngular/diretivasAngular/src/app/app.module.ts
+++ diretivasAngular/diretivasAngular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { MarcaTextoDirective } from './directives/marca-texto.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarcaTextoDirective
   ],
   imports: [
     BrowserModule,

15a7fecb135af8c70ae743f5011ef3417a9fc27b
kanaiama <wilson.kanaiamajr@sptech.school>
2023-04-20 14:32:34 -0300

    CREATE: list api + add listagem personagens ricky and morty

--- diretivasAngular/diretivasAngular/src/app/app.module.ts
+++ diretivasAngular/diretivasAngular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,17 +8,20 @@ import { FormsModule } from '@angular/forms';
 import { MarcaTextoDirective } from './directives/marca-texto.directive';
 import { PeopleService } from './shared/services/people.service';
 import { ListPeopleComponent } from './shared/components/list-people/list-people.component';
+import { ListApiComponent } from './components/list-api/list-api.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MarcaTextoDirective,
-    ListPeopleComponent
+    ListPeopleComponent,
+    ListApiComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [PeopleService],
   bootstrap: [AppComponent]
